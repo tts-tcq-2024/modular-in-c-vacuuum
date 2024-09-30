6b7455a635cbdcbc5c8da70d9d6f6b3f4e649850
vacuuum <47915224+vacuuum@users.noreply.github.com>
2024-09-30 16:48:03 +0530

    Update test_WiringReferenceManual.cpp

--- test_WiringReferenceManual.cpp
+++ test_WiringReferenceManual.cpp
@@ -1,3 +1,28 @@
 #include <stdio.h>
 #include <assert.h> 
 #include <WiringReferenceManual.h>
+
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
