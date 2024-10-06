b4b4e9bc283840fb214cf98fe6c01d4f37c5468a
vacuuum <47915224+vacuuum@users.noreply.github.com>
2024-10-07 00:05:53 +0530

    Update WiringReferenceManual.h

--- WiringReferenceManual.h
+++ WiringReferenceManual.h
@@ -1,19 +1,32 @@
+#ifndef WIRING_REFERENCE_MANUAL_H
+#define WIRING_REFERENCE_MANUAL_H
+
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Function declarations
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void PrintWiringReferenceManual(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // WIRING_REFERENCE_MANUAL_H
