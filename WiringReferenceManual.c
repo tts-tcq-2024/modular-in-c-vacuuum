9be4c750970a59bad85bd3bb703ce8af5cca09dd
vacuuum <47915224+vacuuum@users.noreply.github.com>
2024-10-07 01:19:31 +0530

    Update WiringReferenceManual.c

--- WiringReferenceManual.c
+++ WiringReferenceManual.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include "WiringReferenceManual.h"
 
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer)
 {
-    sprintf(buffer, "%s %s",
-        MajorColorNames[colorPair->majorColor],
-        MinorColorNames[colorPair->minorColor]);
+    sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor],MinorColorNames[colorPair->minorColor]);
 }
 
 ColorPair GetColorFromPairNumber(int pairNumber)
 {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
     return colorPair;
 }
 
 int GetPairNumberFromColor(const ColorPair* colorPair)
 {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
+    return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
 }
 
 void PrintWiringReferenceManual(void)
@@ -34,4 +34,14 @@ void PrintWiringReferenceManual(void)
         ColorPairToString(&colorPair, colorPairNames);
         printf("%d\t%s\n", pairNumber, colorPairNames);
     }
+}
+
+int main()
+{
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+    PrintWiringReferenceManual();
+    return 0;
 }
