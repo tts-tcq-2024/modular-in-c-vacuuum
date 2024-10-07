bb982ad5266d2714f6c944cfc647ae079ec2fa9e
vacuuum <47915224+vacuuum@users.noreply.github.com>
2024-10-07 09:37:51 +0530

    Update WiringReferenceManual.c

--- WiringReferenceManual.c
+++ WiringReferenceManual.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "WiringReferenceManual.h"
-#include "test_WiringReferenceManual.h" 
 
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
