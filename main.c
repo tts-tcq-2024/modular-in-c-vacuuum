0c3d9d7e5409e270dcddd39222657dd39f9ff19a
vacuuum <47915224+vacuuum@users.noreply.github.com>
2024-10-07 10:23:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,6 @@ int main()
     testNumberToPair(5, WHITE, SLATE);
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-    PrintWiringReferenceManual();
+    FormatWiringReferenceManual();
     return 0;
 }
