3ff7e3c328bb35977b15b7a1d3bb947c895c4675
vacuuum <47915224+vacuuum@users.noreply.github.com>
2024-10-07 10:13:34 +0530

    Update WiringReferenceManual_test.h

--- WiringReferenceManual_test.h
+++ WiringReferenceManual_test.h
@@ -1,17 +1,8 @@
 #ifndef TEST_WIRING_REFERENCE_MANUAL_H
 #define TEST_WIRING_REFERENCE_MANUAL_H
-
 #include "WiringReferenceManual.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // TEST_WIRING_REFERENCE_MANUAL_H
